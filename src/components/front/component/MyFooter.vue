811854ba8a47f206ac7c212374af3e3e7965ebb8
毛瑞彬 <www.maoruibin@163.com>
2019-08-19 16:07:10 +0800

    chore: 添加备案号

--- src/components/front/component/MyFooter.vue
+++ src/components/front/component/MyFooter.vue
@@ -2,10 +2,14 @@
   <div class="footer">
     <!-- <a href="https://segmentfault.com/u/fatdong1" class="iconfont icon-iconsf" /> -->
     <!-- <a href="http://www.facebook.com/fatdong1" class="iconfont icon-cion15" /> -->
-    <p>qq号：595123108 <a class="iconfont icon-github" href="https://github.com/maoruibin001" /></p>
     <p>
-      Copyright @ 2019 Powered by Vue. |
-      <router-link :to="{name: 'login'}" tag="span" class="login">
+      qq号：595123108 <a class="iconfont icon-github"
+href="https://github.com/maoruibin001" />
+    </p>
+    <p>
+      <a href="http://beian.miit.gov.cn/">Copyright @ 2019 Powered by Vue. |</a>
+      <router-link :to="{name: 'login'}"
+tag="span" class="login">
         站长登录
       </router-link>
     </p>
@@ -13,41 +17,48 @@
 </template>
 
 <script>
-  export default {}
+export default {}
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-  .iconfont {
+.iconfont {
     font-size: 1.875rem;
     color: #fff;
     margin-right: 0.625rem;
-  }
-  .icon-github:hover {
+}
+
+.icon-github:hover {
     color: royalblue;
-  }
-  .icon-cion15 {
+}
+
+.icon-cion15 {
     font-size: 1.625rem;
-  }
-  .icon-cion15:hover {
+}
+
+.icon-cion15:hover {
     color: deepskyblue;
-  }
-  .icon-iconsf:hover {
+}
+
+.icon-iconsf:hover {
     color: green;
-  }
-  div {
+}
+
+div {
     text-align: center;
     padding-top: 0.625rem;
     line-height: 2.5rem;
     width: 100%;
     background: rgba(55, 57, 65, 0.5);
-  }
-  p {
+}
+
+p {
     text-align: center;
     color: #ffffff;
     font-size: 1rem;
-  }
-  span.login {
+}
+
+span.login {
     cursor: pointer;
     color: #fff;
-  }
+}
 </style>
